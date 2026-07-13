1d4cd6b7f8aa0a8736a47f4f4dc048d12caef1cd
Dinuka <infodinukalk@gmail.com>
2026-07-13 21:55:17 +0530

    export inlineNotice

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,6 +2,7 @@ export * from './Button';
 export * from './Card';
 export * from './EmptyState';
 export * from './ErrorState';
+export * from './InlineNotice';
 export * from './PlaceholderScreen';
 export * from './Screen';
 export * from './StatusBadge';
